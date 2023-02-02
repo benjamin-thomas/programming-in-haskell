59604d5fe9ee2cf7a9e368534c723e72ec03567c
Benjamin <benjamin.guy.thomas@gmail.com>
2023-02-02 07:26:19 +0100

    Add quicksort

--- src/Chapter01.hs
+++ src/Chapter01.hs
@@ -87,8 +87,51 @@ sum' [1, 2, 3] = 6
 
 
 The type signature `Num a => [a] -> a` means:
-For any number of type `a`, transform a list of `a`s to an `a`.
+For any number (int, float, etc.) of type `a`, transform a list of `a`s to an `a`.
  -}
 sum' :: Num a => [a] -> a
 sum' [] = 0
 sum' (n:ns) = n + sum' ns
+
+
+{- | Quick sort implementation
+
+>>> qsort [3, 5, 1, 4, 2]
+[1,2,3,4,5]
+
+>>> qsort "cad"
+"acd"
+
+The type sig `Ord a => [a] -> [a]` is more general than the prior (see `sum'`)
+
+`Ord` is short for ordered values, such as ints, floats, strings (list of chars), list of strings, etc.
+
+`++` is the list append operator.
+
+`smaller = [a | a <- xs, a <= x]` means:
+  smaller is a list of `a`s, sourced from `xs` and for which its elements are <= to `x`
+
+Here is (roughly) how the function application occurs:
+
+One element
+===========
+
+qsort [1] = qsort [] ++ [1] ++ qsort []
+qsort [1] = [] ++ [1] ++ []
+qsort [1] = [1]
+
+Many elements
+=============
+
+qsort [3, 5, 1, 4, 2] = (qsort [1, 2]) ++ [3] ++ (qsort [5, 4])
+qsort [3, 5, 1, 4, 2] = ( qsort [] ++ [1] ++ qsort [2]  ) ++ [3] ++ (qsort [4] ++ [5] ++ qsort [])
+qsort [3, 5, 1, 4, 2] = ( [] ++ [1] ++ [2]  ) ++ [3] ++ ([4] ++ [5] ++ [])
+qsort [3, 5, 1, 4, 2] = ([1, 2]) ++ [3] ++ ([4, 5])
+qsort [3, 5, 1, 4, 2] = [1, 2, 3, 4, 5]
+ -}
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort greater
+               where
+                   smaller = [a | a <- xs, a <= x]
+                   greater = [a | a <- xs, a  > x]

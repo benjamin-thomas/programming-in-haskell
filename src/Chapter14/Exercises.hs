60a4e9f0b63932650f059f42db37b94395c2cb38
Benjamin Thomas <benjamin.guy.thomas@gmail.com>
2026-08-02 23:13:08 +0200

    Finish Chapter 14 exercises!

--- src/Chapter14/Exercises.hs
+++ src/Chapter14/Exercises.hs
@@ -2,6 +2,12 @@
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
+{- HLINT ignore "Use list comprehension" -}
+
+{- HLINT ignore "Use id" -}
+
+{- HLINT ignore "Use :" -}
+
 {- HLINT ignore "Use <$>" -}
 
 {- HLINT ignore "Use sequenceA" -}
@@ -10,20 +16,29 @@
 
 module Chapter14.Exercises where
 
-import Data.List ((++))
+import Data.List (filter, (++))
 import Data.String
 import Prelude
     ( Applicative
+    , Bool (..)
     , Functor
+    , Int
     , Show
+    , const
+    , even
+    , flip
     , fmap
     , id
-    , length
+    , not
     , pure
     , undefined
+    , ($)
+    , (*)
     , (+)
+    , (-)
     , (.)
     , (<*>)
+    , (>)
     )
 
 {-
@@ -45,16 +60,16 @@ class (Semigroup a) => Monoid a where
     mappend :: a -> a -> a
     mappend = (<>)
 
-instance Semigroup String where
-    (<>) :: String -> String -> String
-    (<>) = (++)
+-- instance Semigroup String where
+--     (<>) :: String -> String -> String
+--     (<>) = (++)
 
-instance Monoid String where
-    mempty :: String
-    mempty = ""
+-- instance Monoid String where
+--     mempty :: String
+--     mempty = ""
 
-    mappend :: String -> String -> String
-    mappend = (<>)
+--     mappend :: String -> String -> String
+--     mappend = (<>)
 
 instance (Monoid a, Monoid b) => Semigroup (a, b) where
     (<>) :: (Monoid a, Monoid b) => (a, b) -> (a, b) -> (a, b)
@@ -110,6 +125,9 @@ class Foldable t where
     foldMap :: (Monoid b) => (a -> b) -> t a -> b
     foldr :: (a -> b -> b) -> b -> t a -> b
     foldl :: (b -> a -> b) -> b -> t a -> b
+
+    toList :: t a -> [a]
+    toList = foldMap (\x -> [x])
 
 instance (Semigroup a) => Semigroup (Maybe a) where
     (<>) :: Maybe a -> Maybe a -> Maybe a
@@ -211,3 +229,162 @@ instance Traversable Maybe where
 [Just 3,Just 13]
 
  -}
+
+{-
+Ex 4:
+
+In a similar manner, show how the following type of binary tree with data in
+their nodes can be made into a foldable and traversable type.
+-}
+data Tree a
+    = Leaf
+    | Node (Tree a) a (Tree a)
+    deriving (Show)
+
+{-
+instance Semigroup a => Semigroup (Tree a) where
+    (<>) :: Tree a -> Tree a -> Tree a
+    (<>) (Node l x r) (Node l' x' r') = Node (l <> l') (x <> x') (r <> r')
+    (<>) l    Leaf = l
+    (<>) Leaf r    = r
+-}
+
+-- Traversable depends on Functor and Foldable
+-- Functor and Foldable depend on nothing
+tree :: Tree Int
+tree =
+    Node
+        ( Node
+            Leaf
+            1
+            Leaf
+        )
+        2
+        ( Node
+            Leaf
+            3
+            Leaf
+        )
+
+tree' :: Tree String
+tree' =
+    Node
+        ( Node
+            Leaf
+            "a"
+            Leaf
+        )
+        "b"
+        ( Node
+            Leaf
+            "c"
+            Leaf
+        )
+
+instance Functor Tree where
+    fmap :: (a -> b) -> Tree a -> Tree b
+    fmap _ Leaf = Leaf
+    fmap f (Node l x r) = Node (fmap f l) (f x) (fmap f r)
+
+{-
+
+\*Chapter14.Exercises> tree
+Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+\*Chapter14.Exercises> fmap (*2) tree
+Node (Node Leaf 2 Leaf) 4 (Node Leaf 6 Leaf)
+\*Chapter14.Exercises> fmap (*2) $ fmap (*2) tree
+Node (Node Leaf 4 Leaf) 8 (Node Leaf 12 Leaf)
+\*Chapter14.Exercises> fmap (*2) $ fmap (*2) $ fmap (*2) tree
+Node (Node Leaf 8 Leaf) 16 (Node Leaf 24 Leaf)
+
+-}
+
+instance Foldable Tree where
+    fold :: (Monoid a) => Tree a -> a
+    fold Leaf = mempty
+    -- Can also be defined as `foldMap id`
+    fold (Node l x r) = fold l <> x <> fold r
+
+    foldMap :: (Monoid b) => (a -> b) -> Tree a -> b
+    foldMap _ Leaf = mempty
+    foldMap f (Node l x r) = foldMap f l <> f x <> foldMap f r
+
+    foldr :: (a -> b -> b) -> b -> Tree a -> b
+    foldr _ acc Leaf = acc
+    foldr f acc (Node l x r) = foldr f (f x (foldr f acc r)) l
+
+    foldl :: (b -> a -> b) -> b -> Tree a -> b
+    foldl _ acc Leaf = acc
+    foldl f acc (Node l x r) = foldl f (f (foldl f acc l) x) r
+
+mTree :: Tree (Maybe Int)
+mTree =
+    Node
+        ( Node
+            Leaf
+            (Just 1)
+            Leaf
+        )
+        (Just 2)
+        ( Node
+            Leaf
+            (Just 3)
+            Leaf
+        )
+
+instance Traversable Tree where
+    sequenceA :: (Applicative f) => Tree (f a) -> f (Tree a)
+    sequenceA Leaf = pure Leaf
+    sequenceA (Node f x r) = pure Node <*> sequenceA f <*> x <*> sequenceA r
+
+instance Applicative Maybe where
+    pure :: a -> Maybe a
+    pure = Just
+
+    (<*>) :: Maybe (a -> b) -> Maybe a -> Maybe b
+    Just f <*> Just x = Just (f x)
+    _ <*> _ = Nothing
+
+{-
+
+\*Chapter14.Exercises> traverse (\x -> if x > 0 then Just x else Nothing) tree
+Just (Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf))
+
+\*Chapter14.Exercises> sequenceA mTree
+Just (Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf))
+
+ -}
+
+{-
+
+Ex 5:
+
+Using [foldMap], define a generic version of the higher-order function [filter]
+on lists that can be used with any foldable type
+
+ -}
+
+instance Semigroup [a] where
+    (<>) :: [a] -> [a] -> [a]
+    xs <> ys = xs ++ ys
+
+instance Monoid [a] where
+    mempty :: [a]
+    mempty = []
+
+-- treeToList :: (Foldable t) => t a -> [a]
+-- treeToList = foldMap (\x -> [x])
+
+{-
+
+>>> filterF even tree
+[2]
+
+>>> filterF (not . even) tree
+[1,3]
+
+ -}
+
+filterF :: (Foldable t) => (a -> Bool) -> t a -> [a]
+-- filterF f = filter f . toList
+filterF f = foldMap (\x -> if f x then [x] else [])
